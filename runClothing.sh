74b4047921a01f33d5d82524f1a2d000b187f858
tschmidt6 <36741011+tschmidt6@users.noreply.github.com>
2018-10-23 18:35:17 -0500

    Add files via upload

---
+++ runClothing.sh
@@ -0,0 +1,6 @@
+# Background.jpg = picture of you 
+# Foreground.png = shirt you want to try on
+
+python fashion.py background1.jpg ./background1mask.png
+python fashion.py background2.jpg ./background2mask.png
+python clothing.py
